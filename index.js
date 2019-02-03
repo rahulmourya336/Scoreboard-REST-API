39758f71def769facc23bad1f5169a54582004ca
rahulmourya336@gmail.com <rahul@yahoo.com>
2019-02-03 19:31:09 +0530

    Fix Error R10 (Boot timeout) Error

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const fastify = require('fastify')({
   logger: true
 });
 
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const path = require('path');
+// const PORT = process.env.PORT || 5000;
 
 // Declare a route
 fastify.get('/', (request, reply) => {
   reply.send({ hello: 'world' })
-})
+});
 
 // Run the server!
-fastify.listen(3000, (err) => {
+fastify.listen(process.env.PORT || 5000, (err) => {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
   }
   fastify.log.info(`server listening on ${fastify.server.address().port}`)
-})
+});
