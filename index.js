ff8744bbc41b647756d7d674e824f2827a005f9c
rahulmourya336@gmail.com <rahul@yahoo.com>
2019-02-03 18:23:37 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// Require the framework and instantiate it
+const fastify = require('fastify')({
+  logger: true
+});
+
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+// Declare a route
+fastify.get('/', (request, reply) => {
+  reply.send({ hello: 'world' })
+})
+
+// Run the server!
+fastify.listen(3000, (err) => {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  fastify.log.info(`server listening on ${fastify.server.address().port}`)
+})
